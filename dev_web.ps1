ed020b1b984b7de19df9f05a2175034ba0380fa6
robpot891 <rpotter@live.ca>
2019-01-21 16:28:57 -0500

    Update dev_web.ps1

--- dev_web.ps1
+++ dev_web.ps1
@@ -26,18 +26,30 @@ executeScript "FileExplorerSettings.ps1";
 executeScript "SystemConfiguration.ps1";
 executeScript "CommonDevTools.ps1";
 executeScript "RemoveDefaultApps.ps1";
-executeScript "HyperV.ps1";
-executeScript "Docker.ps1";
 executeScript "WSL.ps1";
 executeScript "Browsers.ps1";
-
+executeScript "GetMLIDEAndTooling.ps1";
+executeScript "PythonMLTools.ps1";
+choco install -y powershell-core
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+#--- File Explorer Settings ---
+# will expand explorer to the actual folder you're in
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+#adds things back in your left pane like recycle bin
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+#opens PC to This PC, not quick access
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+#taskbar where window is open for multi-monitor
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
 #--- Tools ---
 code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension msjsdiag.debugger-for-edge
-
-#--- Microsoft WebDriver ---
-choco install -y microsoftwebdriver
-
-Enable-UAC
+choco install -y sysinternals
+scoop install aria2
+scoop install ssh coreutils virtualbox vagrant nodejs 
+executeScript "GetMLIDEAndTooling.ps1";
+executeScript "PythonMLTools.ps1";
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+#Enable-UAC
 Enable-MicrosoftUpdate
 Install-WindowsUpdate -acceptEula
